9e94a255a34eb4e766be69670f27982e163b02a5
Cxmu03 <Freunscht58@outlook.de>
2024-01-21 02:52:50 +0100

    Added script to generate instruction table

---
+++ scripts/generate_instructions.py
@@ -0,0 +1,44 @@
+import json
+
+with open("data/instructions.json", "r") as f:
+    instructions = json.load(f)
+
+invalid_opcodes = instructions["invalid_opcodes"]
+instruction_list = instructions["instructions"]
+addressing_modes = instructions["addressing_modes"]
+
+def valid(opcode: int, instruction: str):
+    mnemonic, addressing_mode = instruction.split(" ")
+    return "0x{}, InstructionType::{}, AddressingMode::{}, {}, {}, Cpu::{};\n".format(
+        f"{opcode:0>2X}",
+        mnemonic,
+        addressing_modes[addressing_mode],
+        0,
+        "false",
+        mnemonic.lower()
+    )
+
+def invalid(opcode):
+    return valid(opcode, "NOP impl")
+
+def generate_instruction_table():
+    current_valid_index = 0
+    for i in range(0x100):
+        if f"{i:0>2X}" in invalid_opcodes:
+            current_instruction = invalid(i)
+        else:
+            current_instruction = valid(i, instruction_list[current_valid_index])
+            current_valid_index += 1
+
+        yield current_instruction
+
+def main():
+    with open("instruction_table.txt", "w") as f:
+        for instruction in generate_instruction_table():
+            # ugly ass hack
+            if instruction.startswith("0xFF"):
+                instruction = instruction[:-2]
+            f.write(instruction)
+
+if __name__ == "__main__":
+    main()
